a900796a511246d1d92ca1836c374ffefe23d474
ArielInguillay <arieling123@gmail.com>
2025-07-11 20:10:06 -0500

    feat: Add protected routes to manage user preferences

--- src/routes/preferencesRoutes.js
+++ src/routes/preferencesRoutes.js
@@ -8,7 +8,7 @@ const {
 
 const verifyJWT = require('../middlewares/verifyJWT');
 
-// ✅ Todas las rutas están protegidas
+//  Todas las rutas están protegidas
 router.get('/me', verifyJWT, getMyPreferences);
 router.put('/me', verifyJWT, updateMyPreferences);
 router.post('/reset', verifyJWT, resetMyPreferences);
