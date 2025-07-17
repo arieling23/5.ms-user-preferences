3373f9561bfb93e33d5d456151fd57cdc43afec6
ArielInguillay <arieling123@gmail.com>
2025-07-17 12:32:53 -0500

    refactor(router): remove explicit CORS preflight routes for /me and /reset

--- src/routes/preferencesRoutes.js
+++ src/routes/preferencesRoutes.js
@@ -10,10 +10,6 @@ const {
 const verifyJWT = require('../middlewares/verifyJWT');
 
 
-router.options('/me', cors());
-router.options('/reset', cors());
-
-
 router.get('/me', verifyJWT, getMyPreferences);
 router.put('/me', verifyJWT, updateMyPreferences);
 router.post('/reset', verifyJWT, resetMyPreferences);
